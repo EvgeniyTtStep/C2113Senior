690742c625987abf5ad1fe746f7657287f038789
Evgeniy <46program@gmail.com>
2024-03-30 16:05:34 +0200

    lesson 7 generator

--- lesson7/generator.py
+++ lesson7/generator.py
@@ -1,7 +1,17 @@
+# def raise_to_the_degrees(num, max_deg):
+#     i = 0
+#     for item in range(max_deg):
+#         yield num ** i
+#         i += 1
+
+
 def raise_to_the_degrees(num, max_deg):
     i = 0
-    for item in range(max_deg):
-        yield num ** i
+    while True:
+        res = num ** i
+        yield res
+        if res > 100 ** 20:
+            return
         i += 1
 
 
